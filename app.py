04bb697b0ec27d4fb8e61579abb96a864662e3c8
Tristan <tristan.perard@outlook.com>
2024-04-10 13:40:59 +0300

    Fix attachment name

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ msg["From"] = "noreply@csc.fi"
 msg["Subject"] = "Broken links report"
 msg["To"] = os.getenv('EMAIL_RECIPIENT')
 msg.set_content("You will find attached the broken links report of docs.csc.fi")
-msg.add_attachment(open(OUTPUTFILE, "r", encoding="utf-8").read())
+msg.add_attachment(open(OUTPUTFILE, "r", encoding="utf-8").read(), subtype='txt', filename='404.txt')
 
 s = smtplib.SMTP('smtp.pouta.csc.fi')
 s.send_message(msg)
